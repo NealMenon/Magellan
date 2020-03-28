fc958b95bbf1963446746d171ce1f51a3478c89e
nealmenon <nealmenon@gmail.com>
2020-03-28 09:44:40 +0530

    Base case and param fix

--- Bot.java
+++ Bot.java
@@ -1,12 +1,21 @@
 
 public class Bot {
-
+    String path;
     public Bot() {
-
+        path = "";
     }
 
-    public void explore(Map map, int X[], int Y[]){
-        System.out.println("Bot is at " + map.X[0] + " " + map.X[1]);
+    public void explore(Map map, int X[], int Y[], int dim, String curJourney){
+        // System.out.println("Bot is at " + map.X[0] + " " + map.X[1]);
+        if(X[0] == Y[0] && X[1] == Y[1]) {
+            // WE HAVE ARRIVED
+            this.path = curJourney;
+            System.out.println("Found by traversing: "+ this.path);
+            return;
+        }
+        if(X[0] + 1 < dim) {
+            // move right
+        }
 
     }
 }
